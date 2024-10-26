3cc4272c14c432afc902022417bdfe990b53e804
CodeTornadoStorms <demyu369@gmail.com>
2024-10-25 20:40:36 -0500

    Integracion con Spring Boot

---
+++ src/test/java/com/reserva/unipamplona/reserva/unipamplona/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserva.unipamplona.reserva.unipamplona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
