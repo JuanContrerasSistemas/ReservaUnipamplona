aef4f56b4ef1ec53959d07a565d680100f8629dc
kevinalej21 <kevi.kc7@gmail.com>
2024-10-29 21:51:14 -0500

    se agrego empoin para elminar usuarios

--- src/main/java/com/reserva/unipamplona/reserva/unipamplona/controller/UsuarioController.java
+++ src/main/java/com/reserva/unipamplona/reserva/unipamplona/controller/UsuarioController.java
@@ -1,14 +1,24 @@
 package com.reserva.unipamplona.reserva.unipamplona.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/usuarios")
 
 public class UsuarioController {
+
+	// empoin para hacer el delete en el postman
+
+	@DeleteMapping("")
+	public String elimianarUsuario(@RequestParam("cedula") int cedula){
+		return "Se elimino el usuario con la cedula : " + cedula ;
+	}
+	
+
 	@PostMapping("/registrar")
 	public String registrarUsuario(@RequestParam("nombre") String nombre, @RequestParam("cedula") String cedula,
 			@RequestParam("telefono") String telefono, @RequestParam("email") String email,
@@ -19,3 +29,4 @@ public class UsuarioController {
 	}
 
 }
+
